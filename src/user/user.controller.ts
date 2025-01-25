1771c37f287a4bf06618ec966132af8a4a6c124f
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-01-25 17:40:09 -0300

    feat: Adicionado Metodo GET findAll

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userServise: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userServise.findAll();
+  }
+
+}
